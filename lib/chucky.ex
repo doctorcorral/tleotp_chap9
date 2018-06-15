598895c42e0c813b164bab1f5fcb77137c24cd26
Ricardo C <ricardo@suggestic.com>
2018-06-14 20:15:01 -0500

    add type case

--- lib/chucky.ex
+++ lib/chucky.ex
@@ -6,9 +6,23 @@ defmodule Chucky do
 
   def start(type, _args) do
     import Supervisor.Spec
+
     children = [
       worker(Chucky.Server, [])
     ]
   end
 
+  case type do
+    :normal ->
+      Logger.info("Application is started on #{node}")
+
+    {:takeover, old_node} ->
+      Logger.info("#{node} is taking over #{old_node}")
+
+    {:failover, old_node} ->
+      Logger.info("#{old_node} is failing over to #{node}")
+  end
+
+  opts = [strategy: :one_for_one, name: {:global, Chucky.Supervisor}]
+  Supervisor.start_link(children, options)
 end
