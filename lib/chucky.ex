9088d41dc187fabae8dcb42aad856d1c65d2f714
Ricardo C <ricardo@suggestic.com>
2018-06-21 11:28:05 -0500

    add fact()

--- lib/chucky.ex
+++ lib/chucky.ex
@@ -10,19 +10,23 @@ defmodule Chucky do
     children = [
       worker(Chucky.Server, [])
     ]
+
+    case type do
+      :normal ->
+        Logger.info("Application is started on #{node}")
+
+      {:takeover, old_node} ->
+        Logger.info("#{node} is taking over #{old_node}")
+
+      {:failover, old_node} ->
+        Logger.info("#{old_node} is failing over to #{node}")
+    end
+
+    opts = [strategy: :one_for_one, name: {:global, Chucky.Supervisor}]
+    Supervisor.start_link(children, opts)
   end
 
-  case type do
-    :normal ->
-      Logger.info("Application is started on #{node}")
-
-    {:takeover, old_node} ->
-      Logger.info("#{node} is taking over #{old_node}")
-
-    {:failover, old_node} ->
-      Logger.info("#{old_node} is failing over to #{node}")
+  def fact do
+    Chucky.Server.fact()
   end
-
-  opts = [strategy: :one_for_one, name: {:global, Chucky.Supervisor}]
-  Supervisor.start_link(children, options)
 end
